305fccc327fd0add9b5e0c8904e9dacff9b176fa
Hermansen <hermansendesigns@gmail.com>
2019-03-30 15:35:30 +0100

    Added basic support for docker-compose in users service

---
+++ services/users/Users/UsersContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Users
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+        }
+    }
+}
--- services/users/Users/Utility/StartupDatabaseInitializer.cs
+++ services/users/Users/Utility/StartupDatabaseInitializer.cs
@@ -3,36 +3,59 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Authentication.Utility
+namespace Users.Utility
 {
     public class StartupDatabaseInitializer
     {
         public static void InitializeDatabase(IServiceCollection services)
         {
-            services.AddDbContext<AuthenticationContext>(opt =>
-            {
-                var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
-                var port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
-                var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
-                var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-                var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+            var env = Environment.GetEnvironmentVariable("RUNTIME_ENV");
 
-                var connectionString = $"Host={host};" +
-                                       $"Port={port};" +
-                                       $"Username={user};" +
-                                       $"Password={password};" +
-                                       $"Database={database}";
-                opt.UseNpgsql(connectionString);
-            });
+            switch (env)
+            {
+                case "PRODUCTION":
+                    services.AddDbContext<UsersContext>(opt =>
+                    {
+                        var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+                        var port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+                        var user = Environment.GetEnvironmentVariable("POSTGRES_USER");
+                        var password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+                        var database = Environment.GetEnvironmentVariable("POSTGRES_DB");
+
+                        var connectionString = $"Host={host};" +
+                                               $"Port={port};" +
+                                               $"Username={user};" +
+                                               $"Password={password};" +
+                                               $"Database={database}";
+                        opt.UseNpgsql(connectionString);
+                    });
+                    break;
+                case "TEST":
+                default:
+                    services.AddDbContext<UsersContext>(ops => { ops.UseInMemoryDatabase(databaseName: "Users"); });
+                    break;
+            }
         }
-        
+
         public static void MigrateDatabase(IApplicationBuilder app)
         {
-            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            var env = Environment.GetEnvironmentVariable("RUNTIME_ENV");
+
+            switch (env)
             {
-                var context = serviceScope.ServiceProvider.GetService<AuthenticationContext>();
-                context.Database.Migrate();
-                context.Database.EnsureCreated();
+                case "PRODUCTION":
+                    using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
+                        .CreateScope())
+                    {
+                        var context = serviceScope.ServiceProvider.GetService<UsersContext>();
+                        context.Database.Migrate();
+                        context.Database.EnsureCreated();
+                    }
+
+                    break;
+                case "TEST":
+                default:
+                    break;
             }
         }
     }
