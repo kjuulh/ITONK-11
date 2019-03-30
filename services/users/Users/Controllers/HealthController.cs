e70b6775b823c44a771f049ac2f1d136164e282b
Hermansen <hermansendesigns@gmail.com>
2019-03-30 16:56:42 +0100

    Added health-check (ping-pong) used by kubectl

--- services/users/Users/Controllers/HealthController.cs
+++ services/users/Users/Controllers/HealthController.cs
@@ -1,40 +1,16 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Users.Controllers
 {
-  [Route("api/[controller]")]
-  public class HealthController : Controller
-  {
-    public HealthController() { }
-
-    // GET api/health
-    [HttpGet("")]
-    public ActionResult<IEnumerable<string>> Gets()
+    [Route("api/[controller]")]
+    public class HealthController : Controller
     {
-      return new string[] { "value1", "value2" };
+        // GET api/health/ping
+        [HttpGet("ping")]
+        public ActionResult<string> Ping()
+        {
+            return Ok("{\"message\": \"pong!\"}");
+        }
     }
-
-    // GET api/health/5
-    [HttpGet("{id}")]
-    public ActionResult<string> GetById(int id)
-    {
-      return "value" + id;
-    }
-
-    // POST api/health
-    [HttpPost("")]
-    public void Post([FromBody] string value) { }
-
-    // PUT api/health/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value) { }
-
-    // DELETE api/health/5
-    [HttpDelete("{id}")]
-    public void DeleteById(int id) { }
-  }
 }
