a8b5723a3df4852bdb717316521b9943b960179d
Hermansen <hermansendesigns@gmail.com>
2019-04-26 21:53:42 +0200

    added migrations

---
+++ services/authentication/Authentication/Migrations/20190426195332_initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Authentication.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authentication",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    Username = table.Column<string>(nullable: true),
+                    Hash = table.Column<string>(nullable: true),
+                    Salt = table.Column<string>(nullable: true),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authentication", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authentication");
+        }
+    }
+}
