ddba07f203b643e3b8eda9c24715e2db72638ba7
hermansen <hermansendev@gmail.com>
2019-05-26 01:33:53 +0200

    Added bases for requesters

---
+++ services/stock-buyer/StockProvider/Services/PaymentService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Threading.Tasks;
+
+namespace StockProvider.Services
+{
+    public interface IProviderService
+    {
+    }
+
+    public class ProviderService : IProviderService
+    {
+
+        public ProviderService()
+        {
+        }
+    }
+}
---
+++ services/stock-seller/StockProvider/Controllers/ProviderController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StockProvider.Services;
+
+namespace StockProvider.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProviderController : ControllerBase
+    {
+        private readonly IProviderService _providerService;
+
+        public ProviderController(IProviderService providerService)
+        {
+            _providerService = providerService;
+        }
+    }
+}
